6df884d4766729a73cf0f5c396f65cc309152e1b
Tia Firing <tia.firing@nav.no>
2021-03-22 08:17:01 +0100

    wip miljøoppsett gcp

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,9 +1,22 @@
 package no.nav.syfo
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
-    val applicationName: String = getEnvVar("NAIS_APP_NAME", "narmesteleder")
+    val applicationName: String = getEnvVar("NAIS_APP_NAME", "narmesteleder"),
+    val clientId: String = getFileAsString("/var/run/secrets/AZURE_CLIENT"),
+    val clientSecret: String = getFileAsString("/var/run/secrets/AZURE_CLIENT_SECRET"),
+    val databaseUsername: String = getEnvVar("NAIS_DATABASE_USERNAME"),
+    val databasePassword: String = getEnvVar("NAIS_DATABASE_PASSWORD"),
+    val dbHost: String = getEnvVar("NAIS_DATABASE_HOST"),
+    val dbPort: String = getEnvVar("NAIS_DATABASE_PORT"),
+    val dbName: String = getEnvVar("NAIS_DATABASE_DATABASE")
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
-        System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
+    System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
+
+fun getFileAsString(filePath: String) = String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8)
