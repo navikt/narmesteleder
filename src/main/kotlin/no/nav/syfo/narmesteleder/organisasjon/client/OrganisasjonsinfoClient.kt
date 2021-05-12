45a231934142f6c216e22d370b341c83aa70943d
Tia Firing <tia.firing@nav.no>
2021-05-12 07:13:10 +0200

    log for debug

--- src/main/kotlin/no/nav/syfo/narmesteleder/organisasjon/client/OrganisasjonsinfoClient.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/organisasjon/client/OrganisasjonsinfoClient.kt
@@ -3,6 +3,7 @@ package no.nav.syfo.narmesteleder.organisasjon.client
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.header
+import no.nav.syfo.log
 import no.nav.syfo.narmesteleder.organisasjon.model.Organisasjonsinfo
 import no.nav.syfo.narmesteleder.organisasjon.redis.OrganisasjonsinfoRedisService
 import no.nav.syfo.narmesteleder.organisasjon.redis.toOrganisasjonsinfo
@@ -17,8 +18,10 @@ class OrganisasjonsinfoClient(
     suspend fun getOrginfo(orgNummer: String): Organisasjonsinfo {
         val organisasjonsinfoFraRedis = getOrganisasjonsinfoFromRedis(orgNummer)
         if (organisasjonsinfoFraRedis != null) {
+            log.info("Traff cache")
             return organisasjonsinfoFraRedis
         }
+        log.info("Henter fra ereg")
         val organisasjonsinfo = httpClient.get<Organisasjonsinfo>("$basePath/ereg/api/v1/organisasjon/$orgNummer/noekkelinfo") {
             header("x-nav-apikey", apiKey)
         }
