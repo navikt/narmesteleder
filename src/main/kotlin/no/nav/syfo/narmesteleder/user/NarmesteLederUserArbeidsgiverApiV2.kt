b355165a5c9395f8a99a355edcbe7a82032e908b
Tia Firing <tia.firing@nav.no>
2021-06-28 10:56:12 +0200

    cleanup

--- src/main/kotlin/no/nav/syfo/narmesteleder/user/NarmesteLederUserArbeidsgiverApiV2.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/user/NarmesteLederUserArbeidsgiverApiV2.kt
@@ -7,7 +7,6 @@ import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.syfo.application.BrukerPrincipal
-import no.nav.syfo.log
 import no.nav.syfo.narmesteleder.NarmesteLederService
 import no.nav.syfo.narmesteleder.leder.model.AnsattResponse
 import no.nav.syfo.narmesteleder.leder.model.toAnsatt
@@ -20,7 +19,6 @@ fun Route.registrerNarmesteLederUserArbeidsgiverApiV2(
     get("/arbeidsgiver/v2/ansatte") {
         val principal: BrukerPrincipal = call.authentication.principal()!!
         val fnr = principal.fnr
-        log.info("Fnr: $fnr")
         val callId = UUID.randomUUID()
         val lederRelasjoner = narmesteLederService.getAnsatte(fnr, callId.toString())
         call.respond(AnsattResponse(lederRelasjoner.map { it.toAnsatt() }))
