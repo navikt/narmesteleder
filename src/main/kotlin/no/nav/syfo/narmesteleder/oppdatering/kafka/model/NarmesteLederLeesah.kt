dc8d14c903583c40f73e3a2a8b64f3a517779394
andreasnilsen <danduras@gmail.com>
2021-10-13 14:19:57 +0200

    Legg til kilde på hvordan narmesteleder ble avbrutt

--- src/main/kotlin/no/nav/syfo/narmesteleder/oppdatering/kafka/model/NarmesteLederLeesah.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/oppdatering/kafka/model/NarmesteLederLeesah.kt
@@ -4,6 +4,13 @@ import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.UUID
 
+const val NY_LEDER = "NY_LEDER"
+const val DEAKTIVERT_ARBEIDSTAKER = "DEAKTIVERT_ARBEIDSTAKER"
+const val DEAKTIVERT_ARBEIDSTAKER_INNSENDT_SYKMELDING = "DEAKTIVERT_ARBEIDSTAKER_INNSENDT_SYKMELDING"
+const val DEAKTIVERT_LEDER = "DEAKTIVERT_LEDER"
+const val DEAKTIVERT_ARBEIDSFORHOLD = "DEAKTIVERT_ARBEIDSFORHOLD"
+const val DEAKTIVERT_NY_LEDER = "DEAKTIVERT_NY_LEDER"
+
 data class NarmesteLederLeesah(
     val narmesteLederId: UUID,
     val fnr: String,
@@ -14,5 +21,6 @@ data class NarmesteLederLeesah(
     val aktivFom: LocalDate,
     val aktivTom: LocalDate?,
     val arbeidsgiverForskutterer: Boolean?,
-    val timestamp: OffsetDateTime
+    val timestamp: OffsetDateTime,
+    val status: String? = null
 )
