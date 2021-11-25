b6aac3c6e652a110a31fa933b1a1746a853ec3f2
andreasnilsen <danduras@gmail.com>
2021-11-25 16:59:39 +0100

    fix error

--- src/main/kotlin/no/nav/syfo/application/exception/ServiceUnavailableException.kt
+++ src/main/kotlin/no/nav/syfo/application/exception/ServiceUnavailableException.kt
@@ -1,3 +1,3 @@
 package no.nav.syfo.application.exception
 
-class ServiceUnavailableException(message: String?) : Throwable(message)
+class ServiceUnavailableException(message: String?) : Exception(message)
