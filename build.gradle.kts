5c7f8c98b6fe327c9b75501336ee578cd46872db
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-09-25 09:04:02 +0200

    Use github-package-registry-mirror.gc.nav.no dropping github username and password to get packages from github-package-registry

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,18 +41,11 @@ application {
     mainClass.set("no.nav.syfo.BootstrapKt")
 }
 
-val githubUser: String by project
-val githubPassword: String by project
-
 repositories {
     mavenCentral()
     maven(url = "https://packages.confluent.io/maven/")
     maven {
-        url = uri("https://maven.pkg.github.com/navikt/syfosm-common")
-        credentials {
-            username = githubUser
-            password = githubPassword
-        }
+        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
     }
 }
 
